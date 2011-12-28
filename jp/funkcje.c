ac699646b1f53b918f837e63787de621d1fc5c07
Piotr Jaszczyszyn <infob@wp.pl>
2011-12-28 15:41:04 +0100

    dodalem nowe funkcje

--- jp/funkcje.c
+++ jp/funkcje.c
@@ -7,11 +7,11 @@ int podaj_liczbe(char *prompt)
     scanf("%d", &n);
 
     if (n < 1) {
-	while (n < 1) {
-	    printf
-		("Blad: liczba mniejsza niz '1'\n Podaj liczbe wieksza niz 1: \n");
-	    scanf("%d", &n);
-	}
+        while (n < 1) {
+            printf
+                ("Blad: liczba mniejsza niz '1'\n Podaj liczbe wieksza niz 1: \n");
+            scanf("%d", &n);
+        }
     }
     return n;
 }
@@ -22,7 +22,7 @@ long double silnia(int a)
     int i = a--;
 
     while (a) {
-	i *= a--;
+        i *= a--;
     }
 
     return i;
@@ -34,10 +34,68 @@ void silnia_n_pierwszych(int x){
 
     while (x > 0) {
 
-	sil = silnia(x);
+        sil = silnia(x);
 
-	printf("Silnia z %d to: %Lf\n", x, sil);
+        printf("Silnia z %d to: %Lf\n", x, sil);
 
-	x--;
+        x--;
     }
 }
+
+void clear_table_int(int *table, int size_table)
+{
+    while (size_table-- > 0)
+        table[size_table] = 0;
+}
+
+int czy_to_znak(int a)
+{
+    if (a > 0 && a < 127)
+        return 1;
+    else
+        return 0;
+}
+
+void wypisz_n_znakow(int n, char *znak)
+{
+    ++n;
+    while (--n)
+        printf(znak);
+    printf("\n");
+}
+
+void wypisz_znaki_ascii(int a)
+{
+    if (a == '\n')
+        printf("\\n");
+    else if (a == '\v')
+        printf("\\v");
+    else if (a == '\t')
+        printf("\\t");
+    else if (a == '\r')
+        printf("\\r");
+    else if (a == '\f')
+        printf("\\f");
+    else if (a == 32)
+        printf("spacja");
+    else
+        printf("%c", a);
+
+    printf("\t");
+}
+
+/* pobierz dane - uzycie w programie:
+pobierz_wynik("Odp: ", &c)
+ - pobiera wpisaną wartość do c */
+
+ void pobierz_libcze(char *prompt, int *px)
+{
+    printf(prompt);
+    scanf("%d", px);
+}
+
+void pobierz_dwa_double(char *prompt, double *px, double *py) {
+  printf(prompt);
+  scanf("%lf %lf", px, py);
+}
+
